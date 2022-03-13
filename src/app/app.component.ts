ab110b0fb786b51147a2868b275d27bd8416f7bb
Juraj Vajda <juraj.vajda@gmail.com>
2022-03-13 12:15:04 -0400

    Initial ap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {nanoid} from 'nanoid';
+import {encode as base64encode} from 'base64-arraybuffer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slas-code-gen';
+  codeVerifier = new FormControl('');
+  codeChallenge = new FormControl('');
+
+  selectText(e: FocusEvent) {
+    const target = e.currentTarget as HTMLInputElement;
+    target?.select();
+  }
+
+  /**
+   * Creates Code Verifier use for PKCE auth flow.
+   *
+   * @returns {String} The 128 character length code verifier.
+   */
+  createCodeVerifier() {
+    return nanoid(128);
+  }
+
+  /**
+   * Creates Code Challenge based on Code Verifier
+   *
+   * @param {String} codeVerifier
+   * @returns {String}
+   */
+  async generateCodeChallenge(codeVerifier: string) {
+    let base64Digest;
+
+    const encoder = new TextEncoder()
+    const data = encoder.encode(codeVerifier)
+    const digest = await window.crypto.subtle.digest('SHA-256', data)
+
+    base64Digest = base64encode(digest)
+
+    return base64Digest.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
+  }
+
+  async generate() {
+    const codeVerifier = this.createCodeVerifier();
+    const codeChallenge = await this.generateCodeChallenge(codeVerifier);
+
+    this.codeVerifier.setValue(codeVerifier);
+    this.codeChallenge.setValue(codeChallenge);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
